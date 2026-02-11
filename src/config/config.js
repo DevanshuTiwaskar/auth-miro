33c5d6bd5da357df2d7b770e00b2b8b99845420e
Devanshu Tiwaskar <devanshutiwaskar@gmail.com>
2026-02-11 19:40:32 +0530

    feat: implement initial Express app with Google OAuth authentication and environment configuration.

--- src/config/config.js
+++ src/config/config.js
@@ -7,7 +7,7 @@ const _config = {
   MONGO_URL: process.env.MONGO_URL,
   GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
   GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
-  FRONTEND_URL: process.env.FRONTEND_URL , // Default to relative paths if not set
+  FRONTEND_URL: process.env.FRONTEND_URL || "", // Default to relative paths if not set
   RABBITMQ_URI: process.env.RABBITMQ_URI,
   JWT_SECRET: process.env.JWT_SECRET,
   
